9d73420c0779e4d863b86414685f627860ceb73a
Wesley Chung <wesley.chung2@gmail.com>
2017-01-21 17:38:16 -0500

    Added file

---
+++ pybackend/ProcessStuff.py
@@ -0,0 +1,20 @@
+import collections
+from matplotlib import pyplot as plt
+import pandas
+import re
+
+### splits returned string into words and computes metrics
+text = "Hello I'm going there. There going me,"
+
+# Remove punctuation except for apostrophe and make lowercase
+text = re.sub(r'[^\w\s\']', '', text)
+words = text.lower().split()
+
+# Count words
+counts = collections.Counter(words)
+
+
+df = pandas.DataFrame.from_dict(counts, orient='index')
+
+df.plot(kind='bar')
+plt.show()
